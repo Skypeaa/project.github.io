980bc4afb11cef1d858be8f6bd1a52bea7846d6d
Skypeaa <joshua.barrios.dit.cvsu@gmail.com>
2023-01-25 00:17:07 +0800

    Added new files

---
+++ Javascript/script.js
@@ -0,0 +1,34 @@
+ 
+// menu end
+
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+ 
+// menu end
+
+// scroll to top end
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#scroll').fadeIn();
+        }else{
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+}); 
+
+// scroll to top end
